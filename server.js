66e4f3e7ae7af22818f1255293389401f8bc7cbd
Vaishnavi Borkar <vb.082cheenu@gmail.com>
2023-11-06 13:06:36 +0530

    new

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello world", success: true });
 });
 
+app.get("/api/user", (req, res) => {
+  res.status(200).json({ message: "This is the api for user", success: true });
+});
+
 app.listen(8000, (req, res) => {
   console.log("Server is running on port 8000");
 });
